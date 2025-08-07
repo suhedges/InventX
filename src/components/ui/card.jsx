585c862c4922bf6cbd99519ad2786a4138e85f08
suhedges <160956126+suhedges@users.noreply.github.com>
2025-08-07 15:16:06 -0500

    Create card.jsx

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function Card({ className = '', ...props }) {
+  return <div className={`rounded-lg border bg-white ${className}`} {...props} />
+}
+
+export function CardHeader({ className = '', ...props }) {
+  return <div className={`p-4 border-b ${className}`} {...props} />
+}
+
+export function CardTitle({ className = '', ...props }) {
+  return <h3 className={`text-lg font-semibold ${className}`} {...props} />
+}
+
+export function CardContent({ className = '', ...props }) {
+  return <div className={`p-4 ${className}`} {...props} />
+}
+
+export function CardFooter({ className = '', ...props }) {
+  return <div className={`p-4 border-t ${className}`} {...props} />
+}
